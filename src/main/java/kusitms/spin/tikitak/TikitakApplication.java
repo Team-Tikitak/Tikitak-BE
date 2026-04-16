a7cd83490b36e215c069d230275fb24e77ae074d
kmg22 <ssukmg22@gmail.com>
2026-04-16 18:56:07 +0900

    chore : initial project setup

---
+++ src/main/java/kusitms/spin/tikitak/TikitakApplication.java
@@ -0,0 +1,13 @@
+package kusitms.spin.tikitak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikitakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikitakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kusitms/spin/tikitak/TikitakApplicationTests.java
@@ -0,0 +1,13 @@
+package kusitms.spin.tikitak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikitakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
