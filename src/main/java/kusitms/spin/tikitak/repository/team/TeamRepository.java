921c7870e25535714cbf2d5fbb3b5688487f8836
HarryBae1011 <06harry@naver.com>
2026-05-12 15:52:02 +0900

    fix: DB 조회후 Team 삭제에서 조건부 벌크 삭제로 변경

--- src/main/java/kusitms/spin/tikitak/repository/team/TeamRepository.java
+++ src/main/java/kusitms/spin/tikitak/repository/team/TeamRepository.java
@@ -4,10 +4,12 @@ import kusitms.spin.tikitak.domain.team.entity.Team;
 import kusitms.spin.tikitak.domain.team.enums.TeamStatus;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,5 +18,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @EntityGraph(attributePaths = {"teamMembers", "teamMembers.member"})
     Optional<Team> findTeamWithTeamMembersById(Long teamId);
 
-    List<Team> findAllByStatusAndDeletedAtBefore(TeamStatus status, LocalDateTime dateTime);
+    @Modifying
+    @Query("DELETE FROM Team t WHERE t.status = :status AND t.deletedAt < :cutoff")
+    void bulkDeleteInactiveTeams(@Param("status") TeamStatus status, @Param("deadline") LocalDateTime cutoff);
 }
