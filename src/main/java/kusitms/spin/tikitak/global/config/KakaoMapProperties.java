60764cb5f6bafb848480b00251474740a91dd2e7
HarryBae1011 <06harry@naver.com>
2026-05-18 13:47:08 +0900

    fix: 필수 설정값 검증

--- src/main/java/kusitms/spin/tikitak/global/config/KakaoMapProperties.java
+++ src/main/java/kusitms/spin/tikitak/global/config/KakaoMapProperties.java
@@ -1,9 +1,13 @@
 package kusitms.spin.tikitak.global.config;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "kakao.map")
 public record KakaoMapProperties(
+        @NotBlank
         String restApiKey
 ) {
 }
