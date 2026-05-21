bcf3c7da51950d7b38641e1b8d69c93db198d8aa
HarryBae1011 <06harry@naver.com>
2026-05-22 01:56:01 +0900

    fix: LocalDate 추가

--- src/main/java/kusitms/spin/tikitak/repository/feed/FeedRepository.java
+++ src/main/java/kusitms/spin/tikitak/repository/feed/FeedRepository.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
