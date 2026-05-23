0c154248637f4ab6ac13fed2752e7a6a6ebba865
kmg22 <ssukmg22@gmail.com>
2026-05-23 12:49:22 +0900

    test(feed): update tag limit overflow case for 12 members

--- src/test/java/kusitms/spin/tikitak/service/feed/FeedServiceTest.java
+++ src/test/java/kusitms/spin/tikitak/service/feed/FeedServiceTest.java
@@ -316,9 +316,9 @@ class FeedServiceTest extends UnitTest {
 	}
 
 	@Test
-	@DisplayName("태그 필터 ID는 중복 제거 후 최대 11개까지 허용한다")
+	@DisplayName("태그 필터 ID는 중복 제거 후 최대 12개까지 허용한다")
 	void listFeedsThrowsWhenTaggedTeamMemberFilterCountExceeded() {
-		List<Long> taggedTeamMemberIds = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L);
+		List<Long> taggedTeamMemberIds = List.of(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L);
 		stubActiveAuthor();
 
 		assertThatThrownBy(() -> feedService.listFeeds(
