e24ad60bc6aee8c441b53852786408881d3364a4
kmg22 <ssukmg22@gmail.com>
2026-05-30 12:26:31 +0900

    feat: 오늘의 질문 seed 목록 교체

--- src/main/java/kusitms/spin/tikitak/global/config/WebConfig.java
+++ src/main/java/kusitms/spin/tikitak/global/config/WebConfig.java
@@ -1,10 +1,7 @@
 package kusitms.spin.tikitak.global.config;
 
-import kusitms.spin.tikitak.global.filter.RequestBodyLoggingFilter;
 import kusitms.spin.tikitak.global.security.CurrentMemberIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
