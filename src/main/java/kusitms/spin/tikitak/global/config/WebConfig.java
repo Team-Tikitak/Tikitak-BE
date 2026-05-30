1c0ae6b21c63e4c214459cb795308c8f81d50435
mingyeong <ssukmg22@gmail.com>
2026-05-30 12:30:51 +0900

    feat : 오늘의 질문 seed 목록 교체 (#102)
    * 로그
    
    * feat: 오늘의 질문 seed 데이터 확장
    
    * feat: 오늘의 질문 seed 목록 교체
    
    ---------
    
    Co-authored-by: HarryBae1011 <06harry@naver.com>

--- src/main/java/kusitms/spin/tikitak/global/config/WebConfig.java
+++ src/main/java/kusitms/spin/tikitak/global/config/WebConfig.java
@@ -1,10 +1,7 @@
 package kusitms.spin.tikitak.global.config;
 
-import kusitms.spin.tikitak.global.filter.RequestBodyLoggingFilter;
 import kusitms.spin.tikitak.global.security.CurrentMemberIdArgumentResolver;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,14 +17,5 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(currentMemberIdArgumentResolver);
-	}
-
-	@Bean
-	public FilterRegistrationBean<RequestBodyLoggingFilter> requestBodyLoggingFilter() {
-		FilterRegistrationBean<RequestBodyLoggingFilter> bean = new FilterRegistrationBean<>();
-		bean.setFilter(new RequestBodyLoggingFilter());
-		bean.addUrlPatterns("/api/v1/teams/*", "/api/v1/teams");
-		bean.setOrder(1);
-		return bean;
 	}
 }
